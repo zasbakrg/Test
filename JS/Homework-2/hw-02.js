c5c79b1b7e90e225ca1dff676cd668507306f2fb
zasbakrg <44742673+zasbakrg@users.noreply.github.com>
2019-05-23 23:25:46 +0300

    Update hw-02.js

--- JS/Homework-2/hw-02.js
+++ JS/Homework-2/hw-02.js
@@ -55,6 +55,7 @@ if (numbers.length) {
 //не кончатся попытки или пока пользователь не нажмет Cancel в prompt.
 
 'use strict'
+
 const passwords = ["qwerty", "111qwe", "123123", "r4nd0mp4zzw0rd"];
 let attemptsLeft = 3,
   inputPasswords;
